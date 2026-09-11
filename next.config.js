394bbe8d015b118b3e50410adc972fd6df829a4b
Bhuwan Bhandari <thebhuone@gmail.com>
2026-09-11 22:59:33 +0545

    Deploy portfolio to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  images: { unoptimized: true },
   allowedDevOrigins: ['192.168.18.57'],
 };
 
